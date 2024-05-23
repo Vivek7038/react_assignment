830b6f188820cc6fccee014451c66ba5e9ba503a
VIvek Chavan <95691809+Vivek7038@users.noreply.github.com>
2024-05-23 14:41:49 +0000

    added useToggle hook logic

---
+++ src/hooks/useToggle.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+const useToggle = (initialState = false) => {
+  const [isToggled, setIsToggled] = useState(initialState);
+  const toggle = () => setIsToggled((prev) => !prev);
+  return [isToggled, toggle];
+};
+
+export default useToggle;
