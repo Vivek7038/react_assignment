aa726a90ce730f0109633accfb9f4cfa7fd664f2
VIvek Chavan <95691809+Vivek7038@users.noreply.github.com>
2024-05-23 14:42:45 +0000

    docs:added documentation for useToggle hook

--- src/hooks/useToggle.jsx
+++ src/hooks/useToggle.jsx
@@ -6,3 +6,11 @@ const useToggle = (initialState = false) => {
 };
 
 export default useToggle;
+
+// Parameters
+// initialState: (Optional) The initial value of the toggle state. Defaults to false.
+// Return Value
+// The useToggle hook returns an array with the following elements:
+
+// boolean: The current state of the toggle.
+// function: A function to toggle the state of the toggle.
