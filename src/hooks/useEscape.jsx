dc52777c897e300df53145daf578564c4c92379b
VIvek Chavan <95691809+Vivek7038@users.noreply.github.com>
2024-05-25 07:55:40 +0000

    updated useEscape hook unnessesary paramters

--- src/hooks/useEscape.jsx
+++ src/hooks/useEscape.jsx
@@ -2,25 +2,20 @@ import { useEffect, useCallback } from "react";
 
 const ESCAPE_KEY = 27;
 
-const useEscape = (callbacks, dependencies = []) => {
-  const handleEscape = useCallback(
-    (event) => {
-      if (event.keyCode === ESCAPE_KEY) {
-        callbacks.forEach((callback) => callback(event));
-      }
-    },
-    [callbacks, ...dependencies],
-  );
-
+const useEscape = (callbacks) => {
+  const handleEscape = (event) => {
+    if (event.keyCode === ESCAPE_KEY) {
+      callbacks.forEach((callbackFunction) => callbackFunction());
+    }
+  };
   useEffect(() => {
     window.addEventListener("keydown", handleEscape);
     return () => window.removeEventListener("keydown", handleEscape);
-  }, [handleEscape, ...dependencies]);
+  }, [handleEscape]);
 };
 
 export default useEscape;
 
-// useEscape([callback],[dependencies])
+// useEscape([callbacks])
 // Parameters
 // callbacks: An array of callback functions to be invoked when the Escape key is pressed.
-// dependencies: (Optional) An array of dependencies that, when changed, will trigger the hook to re-register the event listener.
